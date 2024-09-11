30d374e952cac1216a906a72de2da23e5ae51fd1
Sunichii <linus9840@gmail.com>
2024-09-11 09:36:03 +0545

    Added Search bar option and changed font styles

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -11,17 +11,20 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-
-  //api key
   final _weatherService = WeatherService("65840199bc18b46ec973aa02230f4ee5");
   Weather? _weather;
+  String _errorMessage = '';
+  final _searchController = TextEditingController();
 
-  //fetch weather
-  _fetchWeather() async {
-    //displaying current city
-    String cityName = await _weatherService.getCurrentCity();
+  // Fetch weather data
+  _fetchWeather({String? cityName}) async {
+    setState(() {
+      _errorMessage = '';
+    });
 
-    //get weather for city
+    //use the current location city if the location is not given
+    cityName ??= await _weatherService.getCurrentCity();
+
     try {
       final weather = await _weatherService.getWeather(cityName);
       setState(() {
@@ -29,12 +32,24 @@ class _WeatherPageState extends State<WeatherPage> {
       });
     } catch (e) {
       print(e);
+      setState(() {
+        //setting error message on city that doesn't exist
+        _errorMessage = 'Sorry, The city does not exist!!';
+      });
     }
   }
 
-  //weather animation
+  // Handle search submission
+  void _onSearch() {
+    final cityName = _searchController.text.trim();
+    if (cityName.isNotEmpty) {
+      _fetchWeather(cityName: cityName);
+    }
+  }
+
+  // Weather animation
   String getWeatherAnimation(String? mainCondition) {
-    if (mainCondition == null) return 'assets/sunny.json'; //default to sunny
+    if (mainCondition == null) return 'assets/sunny.json'; // Default to sunny
 
     switch (mainCondition.toLowerCase()) {
       case 'clouds':
@@ -57,69 +72,115 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
-  //init state
   @override
   void initState() {
     super.initState();
-    //fetching weather on startup
-    _fetchWeather();
+    _fetchWeather(); // Fetching weather on startup
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // background color
-      body: Center(
+      backgroundColor: Colors.black, // Background color
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //city name
-            Column(
-              children: [
-                Icon(
-                  Icons.location_on,
-                  color: Colors.white,
-                  size: 50,
-                ),
-                Text(
-                  _weather?.cityName ?? "Loading city..",
-                  style: TextStyle(
-                    fontFamily: "BebasNeue",
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white, // text color
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _searchController,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Search for a city...',
+                  hintStyle: const TextStyle(color: Colors.white54, fontFamily: "Poppins"),
+                  prefixIcon: const Icon(Icons.search, color: Colors.white54),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.white54),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            //animation
-            Flexible(
-              child: Lottie.asset(
-                getWeatherAnimation(_weather?.mainCondition),
-                width: 350,
-                height: 350,
+                onSubmitted: (_) => _onSearch(),
               ),
             ),
-            SizedBox(height: 20),
-            //temperature
-            Text(
-              '${_weather?.temperature.round()}°C',
-              style: TextStyle(
-                fontFamily: "BebasNeue",
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                color: Colors.white, // text color
+            if (_errorMessage.isNotEmpty) ...[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  _errorMessage,
+                  style: const TextStyle(
+                    fontFamily: "Poppins",
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            //condition
-            Text(
-              _weather?.mainCondition ?? "Unknown",
-              style: TextStyle(
-                fontFamily: "BebasNeue",
-                fontSize: 25,
-                color: Colors.white54, // text color
+            ],
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (_weather != null) ...[
+                      // City name
+                      Column(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                          Text(
+                            _weather?.cityName ?? "Loading city...",
+                            style: const TextStyle(
+                              fontFamily: "BonaNova",
+                              fontSize: 55,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white, // Text color
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      // Animation
+                      Flexible(
+                        child: Lottie.asset(
+                          getWeatherAnimation(_weather?.mainCondition),
+                          width: 350,
+                          height: 350,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Temperature
+                      Text(
+                        '${_weather?.temperature.round()}°C',
+                        style: const TextStyle(
+                          fontFamily: "BebasNeue",
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white, // Text color
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // Condition
+                      Text(
+                        _weather?.mainCondition ?? "Unknown",
+                        style: const TextStyle(
+                          fontFamily: "BonaNova",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.white54, // Text color
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
               ),
             ),
           ],
